95b80a7a81a849fa593d0ddbda9e596f9d7c0aa3
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-05-19 12:35:00 +0530

    Update avik.sql

--- avik.sql
+++ avik.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2021 at 11:27 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.27
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +11,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prasun`
 --
 
 -- --------------------------------------------------------
